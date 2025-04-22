ba7abf636d55b0f92d86e78b2b2c4182a958bcf5
asinsuaste <camposteven1@gmail.com>
2025-04-22 00:40:17 -0500

    El Boton modifica la imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,3 +1,7 @@
 saludar=function(){
+    let cmpImagenSaludo;
+    cmpImagenSaludo=document.getElementById("imgSaludo");
+    cmpImagenSaludo.src="./Imagenes/giphy.gif"
     console.log("Ingresa a saludar ");
+    
 }

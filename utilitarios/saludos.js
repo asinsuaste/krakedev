df6a98674b877547cb92f559148266b80381bd3a
asinsuaste <camposteven1@gmail.com>
2025-04-22 22:21:56 -0500

    Se implemento las funciones recuperar Inti y recuperar Float

--- utilitarios/saludos.js
+++ utilitarios/saludos.js
@@ -2,7 +2,11 @@ saludar=function(){
 //Recuperar el valor de la caja de texto Nombre
 let nombre=recuperarTexto("txtNombre");
 //Recuperar el valor de la caja de texto Apellido
-let apellido=recuperarTexto("txtApellido");   
+let apellido=recuperarTexto("txtApellido");
+//Recuperar el valor de la caja de texto Edad
+let Edad=recuperarTexto("txtEdad");
+//Recuperar el valor de la caja de texto Estatura
+let aEstatura=recuperarTexto("txtEstatura");  
 }
 
 recuperarTexto=function(idComponente){
@@ -11,4 +15,16 @@ recuperarTexto=function(idComponente){
     componente=document.getElementById(idComponente);
     valorIngresado=componente.value;
     return valorIngresado;
+}
+
+recuperarInt=function(idComponente){
+    let valorCaja=recuperarTexto(idComponente);
+    let valorEntero=parseInt(valorCaja);
+    return valorEntero;
+}
+
+recuperarInt=function(idComponente){
+    let valorCaja=recuperarTexto(idComponente);
+    let valorDecimal=parseFloat(valorCaja);
+    return valorDecimal;
 }

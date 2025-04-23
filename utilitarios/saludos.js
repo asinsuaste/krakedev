e03edfd18a74bc16fde75012b7e73b899ecd2141
asinsuaste <camposteven1@gmail.com>
2025-04-22 23:57:38 -0500

    Se separa las funciones al archivo Utilitarios.js

--- utilitarios/saludos.js
+++ utilitarios/saludos.js
@@ -1,60 +1,21 @@
-saludar=function(){
-//Recuperar el valor de la caja de texto Nombre
-let nombre=recuperarTexto("txtNombre");
-//Recuperar el valor de la caja de texto Apellido
-let apellido=recuperarTexto("txtApellido");
-//Recuperar el valor de la caja de texto Edad
-let Edad=recuperarTexto("txtEdad");
-//Recuperar el valor de la caja de texto Estatura
-let Estatura=recuperarTexto("txtEstatura"); 
-//Mostrar menjaje en pantalla
-let mensajeBienvenida= "Bienvenido "+nombre+" "+apellido;
-
-mostrarTexto("lblResultado", mensajeBienvenida);
-
-mostrarImagen("imgSaludo","./imagenes/homero.gif");
-
-mostrarTextoEnCaja("txtNombre","");
-mostrarTextoEnCaja("txtApellido","");
-mostrarTextoEnCaja("txtEdad","");
-mostrarTextoEnCaja("txtEstatura","");
-}
-
-mostrarImagen=function(idComponente,rutaImagen){
-    let componente=document.getElementById(idComponente);
-    componente.src=rutaImagen;
-    return rutaImagen;
-}
-
-recuperarTexto=function(idComponente){
-    let componente;
-    let valorIngresado;
-    componente=document.getElementById(idComponente);
-    valorIngresado=componente.value;
-    return valorIngresado;
-}
-
-
-recuperarInt=function(idComponente){
-    let valorCaja=recuperarTexto(idComponente);
-    let valorEntero=parseInt(valorCaja);
-    return valorEntero;
-}
-
-recuperarInt=function(idComponente){
-    let valorCaja=recuperarTexto(idComponente);
-    let valorDecimal=parseFloat(valorCaja);
-    return valorDecimal;
-}
-
-mostrarTexto=function(idComponente,mensaje){
-    let componente=document.getElementById(idComponente);
-    componente.innerText=mensaje;
-    return mensaje;
-}
-
-mostrarTextoEnCaja=function(idComponente,mensaje){
-    let componente=document.getElementById(idComponente);
-    componente.value=mensaje;
-    return mensaje;
+saludo=function(){
+    //recuperar el valor de la caja de texto nombre
+    let nombre;
+    nombre=recuperarTexto("txtNombre");
+    //recuperar el valor de la caja de texto apellido
+    let apellido;
+    apellido=recuperarTexto("txtApellido");
+    //recuperar el valor de la caja de texto edad
+    let edad;
+    edad=recuperarInt("txtEdad");
+    //recuperar el valor de la caja de texto estatura
+    let estatura;
+     estatura=recuperarFloat("txtEstatura");
+    //mostrar mensaje en pantalla
+    let mensajeBienvenido="Bienvenido "+nombre+" "+apellido;
+    mostrarTexto("ldlResultado", mensajeBienvenido);
+    //mostrar imagen en pantalla
+    mostrarImagen("imgSaludo","./imagenes/homero.gif");
+    // limpiar caja de texto nombre
+    mostrarTextoEnCaja("txtNombre","");
 }

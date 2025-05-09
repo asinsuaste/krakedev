cac4b6e784e3117561a9e4819d35bcee895e15a6
asinsuaste <camposteven1@gmail.com>
2025-05-09 00:53:24 -0500

    Reto 37 Ejercicio For COMPLETO

---
+++ bucles/ejemplosFor.js
@@ -0,0 +1,58 @@
+mostrarNumeros1=function(){
+    console.log("antes del for");
+    for(let i=0; i<4;i++){
+        console.log(i);
+    }
+
+
+    console.log("despues del for");
+}
+
+mostrarNumeros2=function(){
+    console.log("antes del for");
+    for( let indice=1;indice<=5;indice++){
+        console.log(indice);
+    }
+
+    console.log("despues del for");
+
+}
+
+mostrarPares=function(){
+    console.log("antes del for");
+    for(let x=2; x<10; x+=2 ){
+        console.log(x);
+    }
+
+    console.log("despues del for");
+
+}
+
+mostrarInverso=function(){
+    console.log("antes del for");
+    for(let i=10; i>=0 ; i--){
+        console.log(i);
+      
+    }
+
+    console.log("despues del for");
+}
+
+hackearNasa=function(){
+    //hackeando  Nasa 0%
+    console.log("antes del for");
+    for(let porcentaje=0;porcentaje<=100;porcentaje+=10){
+        console.log("HAKEANDO "+porcentaje+"% ");
+}
+console.log("LA NASA HA SIDO HACKEADA XD"); 
+
+}
+
+mostrarImpares=function(){
+    console.log("antes del for");
+    for(let impares=1; impares<=21;impares+=2){
+        console.log(impares);
+    }
+    console.log("despues del for");
+
+}

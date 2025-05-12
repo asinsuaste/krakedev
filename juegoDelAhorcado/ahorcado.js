547ef5b7483212bcba165829dd9d0cc29135d5b6
asinsuaste <camposteven1@gmail.com>
2025-05-12 01:21:26 -0500

    Funcion ingresarLetra Funciona

--- juegoDelAhorcado/ahorcado.js
+++ juegoDelAhorcado/ahorcado.js
@@ -1,29 +1,57 @@
-//No se olvide de respirar, mantenga la calma y demuestre lo que sabe
-prueba=function(){
-    guardarPalabra();
-
-}
+let palabraSecreta="";
 
 esMayuscula = function(caracter) {
     for (let i = 0; i < caracter.length; i++) {
         if (validarMayuscula(caracter.charCodeAt(i))) {
             return true;
-        }else{
+        } else {
             return false;
-        }
+     }
     }
-    
 }
 
 guardarPalabra=function(){
-    let palabra=recuperarTexto("txtLetra");
+    let palabra=recuperarTexto("txtSecreta");
     let condicional = false; 
 
     if(palabra.length == 5 && esMayuscula(palabra)){
-        console.log(palabra);
+        palabraSecreta=palabra
+        console.log(palabraSecreta);
         condicional = true;
     }else{
         alert("LA PALABRA DEBE TENER 5 LETRAS MAYUSCULAS");
     }
 }
 
+mostrarLetra = function(letra, posicion) {
+    let idDiv = "div" + posicion;
+    mostrarTexto(idDiv, letra);
+}
+
+validar = function(letra) {
+    let letrasEncontradas;
+    let existeLetra = false;
+
+    for (let i = 0; i < palabraSecreta.length; i++) {
+            letrasEncontradas=palabraSecreta.charAt(i);
+        if (letrasEncontradas == letra) {
+            mostrarLetra(letra, i);
+            existeLetra = true;
+        }
+    }
+    if (!existeLetra) {
+        console.log("Letra incorrecta");
+    }
+}
+
+ingresarLetra=function(){
+    let palabra;
+    palabra=recuperarTexto("txtLetra");
+
+    if(esMayuscula(palabra)){
+        validar(palabra);
+    }else{
+        alert("SOLO SE ACEPTAN MAYUSCULAS");
+        validar("");
+    }
+}

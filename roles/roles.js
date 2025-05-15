eb1f3ed0600bb3e322e3e129da2a51f17639bb81
asinsuaste <camposteven1@gmail.com>
2025-05-14 19:14:30 -0500

    Se agrego la funcion mostrasEmpleados, Funciona

--- roles/roles.js
+++ roles/roles.js
@@ -1,7 +1,32 @@
 let empleados = [
     {cedula:"1714616123",nombre:"John",apellido:"Cena",sueldo:500.0},
-    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0}
+    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0},
+    {cedula:"1725621393",nombre:"Anthony",apellido:"Insuaste",sueldo:500.0},
+    {cedula:"1714225107",nombre:"Lourdes",apellido:"Campo",sueldo:900.0}
 ]
+
+mostrasEmpleados=function(){
+    let cmpTabla=document.getElementById("tablaEmpleados");
+    let contenidoTabla="<table><tr>"+
+    "<th>CEDULA</th>"+
+    "<th>NOMBRE</th>"+
+    "<th>APELLIDO</th>"+
+    "<th>SUELDO</th>"+
+    "</tr>";
+    let elementoEmpleados;
+    for(let i=0; i<empleados.length;i++){
+        elementoEmpleados=empleados[i];
+        contenidoTabla+=
+        "<tr><td>"+elementoEmpleados.cedula+"</td>"+
+        "<td>"+elementoEmpleados.nombre+"</td>"+
+        "<td>"+elementoEmpleados.apellido+"</td>"+
+        "<td>"+elementoEmpleados.sueldo+"</td>"
+        +"</tr>";
+    }
+    contenidoTabla+="</table>";
+    cmpTabla.innerHTML=contenidoTabla;
+
+}
 
 mostrarOpcionEmpleado=function(){
     let empleado = "divEmpleado";
@@ -11,7 +36,9 @@ mostrarOpcionEmpleado=function(){
     if ( mostrarComponente(empleado) == mostrarComponente(empleado) ){
         ocultarComponente(rol);
         ocultarComponente(resumen);
+        mostrasEmpleados();
     }
+    
 }
 
 mostrarOpcionRol=function(){
